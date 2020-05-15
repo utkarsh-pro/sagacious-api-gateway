0a6580bab33e2edb7f6405da78d467109bf248c7
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2020-05-16 02:47:43 +0530

    Added key-management directory for handling keys

---
+++ src/key-management/gateway-key.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+let pubKey: string;
+
+if (process.env.NODE_ENV === "production") {
+    // Get the public key from the auth service
+}
+
+
+pubKey = readFileSync(join(__dirname, "keys", "development", "public.key"), 'utf-8')
+
+
+export const publicKey = pubKey;
