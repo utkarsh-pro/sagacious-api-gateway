0eda3f34f41904a59559f3450ef583d7ed86c2ea
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2020-05-15 05:55:02 +0530

    Added the capability to spin up a test server and perform tests

--- src/Gateway/gateway.integration.spec.ts
+++ src/Gateway/gateway.integration.spec.ts
@@ -1,6 +1,8 @@
 import 'mocha'
 import chai from 'chai'
 import chaiHTTP from 'chai-http'
+import { fork } from 'child_process'
+import path from 'path'
 
 import { sign } from 'jsonwebtoken'
 import app from '../index'
@@ -26,6 +28,11 @@ const jwtSignOptions = {
     subject: DummyUser.id
 }
 
+// Spin the test server
+const proc = fork(path.join(__dirname, "dummy-server.js"))
+
+const token = sign(DummyUser, privateKey, { ...jwtSignOptions, algorithm: "RS256" })
+
 describe('Proxy routes integration test', () => {
     describe("GET /api", () => {
         it("should send status 401 when no token is passed", async () => {
@@ -38,7 +45,7 @@ describe('Proxy routes integration test', () => {
         })
 
         it("should send status 200 when token is passed in 'Authorization' header", async () => {
-            const token = sign(DummyUser, privateKey, { ...jwtSignOptions, algorithm: "RS256" })
+
             try {
                 const res = await chai.request(app)
                     .get("/api")
@@ -51,8 +58,32 @@ describe('Proxy routes integration test', () => {
             }
         })
 
+        it("should send status 200 when token is passed in 'x-auth-token' header", async () => {
+            try {
+                const res = await chai.request(app)
+                    .get("/api")
+                    .set("x-auth-token", `${token}`)
+                    .set("x-auth-client", DummyUser.id)
+                    .send()
+                expect(res).to.have.status(200)
+            } catch (error) {
+                throw error
+            }
+        })
+
+        it("should send status 200 when token is passed in uri", async () => {
+            try {
+                const res = await chai.request(app)
+                    .get("/api?token=" + token)
+                    .set("x-auth-client", DummyUser.id)
+                    .send()
+                expect(res).to.have.status(200)
+            } catch (error) {
+                throw error
+            }
+        })
+
         it("should send status 401 when x-auth-client is not passed", async () => {
-            const token = sign(DummyUser, privateKey, { ...jwtSignOptions, algorithm: "RS256" })
             try {
                 const res = await chai.request(app).get("/api").set("Authorization", `Bearer ${token}`).send()
                 expect(res).to.have.status(401)
@@ -61,8 +92,7 @@ describe('Proxy routes integration test', () => {
             }
         })
 
-        it("should send status 401 when x-auth-client is invalid", async () => {
-            const token = sign(DummyUser, privateKey, { ...jwtSignOptions, algorithm: "RS256" })
+        it("should send status 403 when x-auth-client is invalid", async () => {
             try {
                 const res = await chai.request(app)
                     .get("/api")
@@ -75,5 +105,10 @@ describe('Proxy routes integration test', () => {
             }
         })
 
+    })
+
+    after(() => {
+        // Kill the temporary process after running all the test
+        proc.kill("SIGTERM")
     })
 })
